bf12bb6ced13dde249693e1dd4ae4d2a63814edf
Neha Singh <96856900+Neha21singh@users.noreply.github.com>
2023-12-12 14:41:27 +0530

    process complete

--- app/project/apis.py
+++ app/project/apis.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body, Request, Form, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from typing import List
-import uuid
+import uuid , json
 
 from app.project.models import Project, ProjectUpdate
 
@@ -25,18 +25,52 @@ async def create_project(request: Request, project: Project = Body(...)):
     
 #List
 @project_api.get("/", response_description="List all projects")
-def list_projects(request: Request,p_SECTORS: str ="any",p_BLOCKS: str ="any"):
+def list_projects(request: Request,SECTORS:str ='',BLOCKS: str =''):
     qry = {}
-    if (p_SECTORS != 'any') :
-      qry['SECTORS'] = p_SECTORS
-
-    if (p_BLOCKS != 'any') :
-       qry['BLOCKS'] = p_BLOCKS
-
-    print (qry)
-    projects = list(request.app.database["projects"].find({}))
+    if 'x-ses' in request.headers:
+        # print ('FILTERING ...')
+        ses = request.headers['x-ses']        
+        j_ctx = json.loads(ses)
+        # qry['BLOCKS'] = str(j_ctx['BLOCKS'])
+    if SECTORS!='':
+        qry['SECTORS'] = SECTORS
+        
+    if BLOCKS!='':
+        qry['BLOCKS'] = BLOCKS
+        
+    projects = list(request.app.database["projects"].find(qry))
     return {"projects":projects}
+'''   
+@admin_user_api.get("/", response_description="List all user")
+def list_user(request: Request,org:str='',ngo:str='',app:str='',role:str=''):
+    # print('org is:',org)
+    # print('ngo is:',ngo)
+    # print('app is:',app)
+    # print('role is:',role)
 
+    qry = {}
+    if 'x-ses' in request.headers:
+        # print ('FILTERING ...')
+        ses = request.headers['x-ses']        
+        j_ctx = json.loads(ses)
+        if (j_ctx['role'] == 'admin'): qry['ngo_id'] = str(j_ctx['ngo_id'])
+    if org!='':
+        qry['org_id'] = org
+        
+    if ngo!='':
+        qry['ngo_id'] = ngo
+        
+    if app!='':
+        qry['app_id'] = app
+        
+    if role!='':
+        qry['role_id'] = role
+    # print("er",qry)
+        
+    users = list(request.app.database["users"].find(qry))
+    # print(users)
+    return {"users":users}
+'''
     
 #Find
 @project_api.get("/{id}", response_description="Get a single project by id", response_model=Project)
